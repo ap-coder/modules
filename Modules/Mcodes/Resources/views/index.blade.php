e30239b008693e65a3ba4746aaca9f1ba5819c97
ap-coder <apcoder2019@gmail.com>
2021-05-18 17:17:22 -0600

    updates. init commit

---
+++ Modules/Mcodes/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('mcodes::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('mcodes.name') !!}
+    </p>
+@endsection
