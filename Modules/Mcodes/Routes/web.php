41bfd414706ec40a9ce73f4efdbedaf57c38919f
ap-coder <apcoder2019@gmail.com>
2021-05-18 17:36:50 -0600

    Update web.php

--- Modules/Mcodes/Routes/web.php
+++ Modules/Mcodes/Routes/web.php
@@ -3,6 +3,7 @@
 /*
 |--------------------------------------------------------------------------
 | Web Routes  https://nwidart.com/laravel-modules/v6/basic-usage/compiling-assets
+ https://github.com/ap-coder/modules
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
