7f436f9e3e2b98c1dc22fb940b9f86f74e26f3bc
ap-coder <apcoder2019@gmail.com>
2021-05-21 16:11:59 -0600

    Update menu.blade.php

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -22,16 +22,6 @@
                 </li>
 
 
-
-
-{{-- base_path('Modules.Mcode.Resources.views.partials.menu') --}}
-
-
-
-  {{--  @include(
-    base_path('modules.mcode.resources.views.partials.menu')
-  )  --}}
-
   @include('mcode::partials.menu')
 
 
