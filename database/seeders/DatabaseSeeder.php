22766d7c92dc02fe522dc2f0d43535d5bb62780b
ap-coder <apcoder2019@gmail.com>
2021-06-09 11:47:57 -0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,18 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        Model::unguard();
 
+        Schema::disableForeignKeyConstraints();
         $this->call(PermissionsTableSeeder::class);
         $this->call(\Modules\Mcode\Database\Seeders\McodePermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
