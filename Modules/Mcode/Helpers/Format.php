11ba283f83fcb726aafcdf73d6e5dd671acf979f
ap-coder <apcoder2019@gmail.com>
2021-06-14 12:59:30 -0600

    Update Format.php

--- Modules/Mcode/Helpers/Format.php
+++ Modules/Mcode/Helpers/Format.php
@@ -4,8 +4,20 @@ namespace Modules\Mcode\Helpers;
 
 class Format
 {
-	public static function combinedSource (string $string) {
-	
-		return $string;
+	public static function combinedSource (Request $request)
+	{
+		$header = chr(1).'Y'.chr(29).chr(2);
+		$pipe = chr(3);
+		$footer = chr(3) . chr(4);
+		
+		$request = str_replace(', ', ',', $request);
+		$request = trim(preg_replace('/\s\s+/', $pipe, $request));
+		$source_string = str_replace(' ', $pipe, $request);
+
+		
+		$source_string = $header . $source_string . $footer;
+		
+		return $source_string;
+		
 	}
 }
