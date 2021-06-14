11612a12d6ea8e65aef909241a0f07cc9669fc56
tpsvishwas78 <tpsvishwas78@gmail.com>
2021-06-15 02:15:13 +0530

    changes

--- Modules/Mcode/Resources/views/site/mcodes/steps/qr-modal.blade.php
+++ Modules/Mcode/Resources/views/site/mcodes/steps/qr-modal.blade.php
@@ -3,7 +3,6 @@
     <button type="button" class="close" data-dismiss="modal">&times;</button>
     <ul class="qr_icons">
       <li><i class="fas fa-share-square"></i></li>
-      <li><img src="{{ url('site/img/seperate.png') }}"></li>
       <li><a target="_blank" href="{{ url('mcode/getPdf') }}"><img src="{{ url('site/img/download.png') }}"></a></li>
       <li><img src="{{ url('site/img/printer.png') }}"></li>
     </ul>
@@ -26,7 +25,4 @@
     </div>
   </div>
 
-  <!-- Modal footer -->
-  <div class="modal-footer">
-    <button type="button" class="finishedBtn">Finished</button>
-  </div>
+  
